df28d1b47b5f8f6d8317328069238d0b9ae48970
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-17 17:59:15 +0200

    Fix lint

--- src/test/resources/sql/table_init.sql
+++ src/test/resources/sql/table_init.sql
@@ -1,25 +1,25 @@
-CREATE TABLE `hibernate_sequence` (
- `next_val` bigint DEFAULT NULL
+CREATE TABLE "hibernate_sequence" (
+ "next_val" bigint DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
 
 
-CREATE TABLE `messages_localized_string` (
- `id` bigint NOT NULL,
+CREATE TABLE "messages_localized_string" (
+ "id" bigint NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
 
 
-CREATE TABLE `messages_selectors` (
- `messages_localized_string_id` bigint NOT NULL,
- `selector` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
- UNIQUE KEY `UK_mi93k9sji8mulpmjfoerxn5f7` (`selector`),
- KEY `FKbqs5x6uxk5akrqevnxtvbn2lp` (`messages_localized_string_id`)
+CREATE TABLE "messages_selectors" (
+ "messages_localized_string_id" bigint NOT NULL,
+ "selector" varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+ UNIQUE KEY "UK_mi93k9sji8mulpmjfoerxn5f7" ("selector"),
+ KEY "FKbqs5x6uxk5akrqevnxtvbn2lp" ("messages_localized_string_id")
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
 
 
-CREATE TABLE `messages_text_map` (
- `messages_localized_string_id` bigint NOT NULL,
- `text` varchar(10000) COLLATE utf8mb4_unicode_520_ci NOT NULL,
- `language` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
- PRIMARY KEY (`messages_localized_string_id`,`language`)
+CREATE TABLE "messages_text_map" (
+ "messages_localized_string_id" bigint NOT NULL,
+ "text" varchar(10000) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+ "language" varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+ PRIMARY KEY ("messages_localized_string_id","language")
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
--- src/test/resources/sql/truncate_all.sql
+++ src/test/resources/sql/truncate_all.sql
@@ -1,7 +1,7 @@
-TRUNCATE TABLE `messages_text_map`
+TRUNCATE TABLE "messages_text_map"
 
-TRUNCATE TABLE `messages_selectors`
+TRUNCATE TABLE "messages_selectors"
 
-TRUNCATE TABLE `messages_localized_string`
+TRUNCATE TABLE "messages_localized_string"
 
-UPDATE `hibernate_sequence` SET `next_val`='1' WHERE 1
+UPDATE "hibernate_sequence" SET next_val='1' WHERE 1
