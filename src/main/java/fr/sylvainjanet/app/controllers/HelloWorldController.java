51d5f7a645ab373a52ce8b4cf67a5a5d09c5dd45
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-19 07:35:37 +0200

    Add data initialisation
    db had to be reset to change column names
    indeed, sql script review showed that keywords were used for
    column names
    this method has to be used since no proper db migration tool (yet) in
    place

--- src/main/java/fr/sylvainjanet/app/controllers/HelloWorldController.java
+++ src/main/java/fr/sylvainjanet/app/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package fr.sylvainjanet.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fr.sylvainjanet.app.config.InitialData;
 import fr.sylvainjanet.app.dtos.StringDto;
+import fr.sylvainjanet.app.entities.LocalizedString;
+import fr.sylvainjanet.app.repo.LocalizedStringRepository;
 
 /**
  * Controller for Hello World.
@@ -34,9 +38,18 @@ public class HelloWorldController {
   @GetMapping("/hello")
   @ResponseBody
   ResponseEntity<StringDto> home() {
+    for (LocalizedString ls : InitialData.INIT_LOCALIZED_STRING) {
+      repo.save(ls);
+    }
     return new ResponseEntity<>(
         new StringDto("Hello World ! - " + environment), HttpStatus.OK);
   }
+
+  /**
+   * Temp.
+   */
+  @Autowired
+  private LocalizedStringRepository repo;
 
   /**
    * Put mapping used for basic testing of the app functionality : the API
